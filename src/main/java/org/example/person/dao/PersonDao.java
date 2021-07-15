2860711644acb1cea69d5f2ecf498564c6f469ba
Viktoryia Tamazian <vikatamazian@gmail.com>
2021-07-15 21:46:24 +0300

    Simple Person Creator (learning Spring MVC, CRUD, REST, some HTML, without DB yet).

---
+++ src/main/java/org/example/person/dao/PersonDao.java
@@ -0,0 +1,45 @@
+package org.example.person.dao;
+
+import org.example.person.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+
+    public void update(int id, Person personUpdated) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(personUpdated.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
+}
